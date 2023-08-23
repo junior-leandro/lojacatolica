1dda0a30ebce4cf0c22cfb4b98914f3321df5dad
Junior Oliveira Leandro <contato_jr@outlook.com>
2023-08-23 11:58:20 -0300

    updates

--- assets/js/galery.js
+++ assets/js/galery.js
@@ -2,38 +2,59 @@ const controls = document.querySelectorAll('.imagem-control');
 
 let currentImagem = 0;
 const imagens = document.querySelectorAll(".imagem");
-const maxImagens =  imagens.length;
+const maxImagens = imagens.length;
 
 const leftButton = document.querySelector(".imagem-arrow-left");
 const rightButton = document.querySelector(".imagem-arrow-right");
 
+/*
 
-controls.forEach(control => {    
+function imagens(, type){
 
-    control.addEventListener('click', () =>{
+    let currentItem = 0;
+    const leftButton = document.querySelector(".arrow-left");
+    const rightButton = document.querySelector(".arrow-right");
+
+    const isLeft = e.target.classList.contains("arrow-left");
+    items = type;
+
+    if (isLeft) {
+        currentItem = currentItem - 4;
+    } else {
+        currentItem = currentItem + 4;
+    }
+
+    items.forEach((item) => item.classList.remove("current-item"));
+
+    items[currentItem].scrollIntoView({
+        behavior: "smooth",
+        inline: "center"
+    });
+
+    items[currentItem].classList.add("current-item");
+
+};
+*/
+controls.forEach(control => {
+
+    control.addEventListener('click', () => {
 
         const isLeft = control.classList.contains("imagem-arrow-left");
-        
-        if (isLeft){
+
+        if (isLeft) {
             currentImagem = currentImagem - 4;
         } else {
             currentImagem = currentImagem + 4;
         }
-        if (currentImagem >= maxImagens){
-            currentImagem = 0;
-        }
-        if (currentImagem < 0) {
-            currentImagem = maxImagens - 1;
-        }     
-       
+
         /* CHECAGEM DOS BOTÕES */
 
-        if (currentImagem == 0){
+        if (currentImagem == 0) {
             leftButton.classList.add('hidden')
         } else {
             leftButton.classList.remove('hidden')
         }
-        if (currentImagem == 8){
+        if (currentImagem == 8) {
             rightButton.classList.add('hidden')
         } else {
             rightButton.classList.remove('hidden')
@@ -42,16 +63,12 @@ controls.forEach(control => {
         imagens.forEach((imagem) => imagem.classList.remove("current-imagem"));
 
         imagens[currentImagem].scrollIntoView({
-            inline:"start",
+            inline: "start",
             behavior: "smooth",
             block: "nearest"
         });
-        console.log (currentImagem, maxImagens)
-        
-    });
+        console.log(currentImagem, maxImagens)
 
-    if (currentImagem == 0){
-        leftButton.classList.add('hidden')
-    }
+    });
 
 });
